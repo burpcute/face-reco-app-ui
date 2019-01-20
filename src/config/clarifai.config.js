5e0d34cc44a933ff8cc110f2e714c468ce3ad486
burpcute <34086558+burpcute@users.noreply.github.com>
2019-01-20 15:32:49 +0330

    Update clarifai.config.js

--- src/config/clarifai.config.js
+++ src/config/clarifai.config.js
@@ -3,7 +3,7 @@ const _API_KEY = process.env.REACT_APP_CLARIFAI_API_KEY;
 
 const ClarifaiConfig = {
   app: new Clarifai.App({
-    apiKey: _API_KEY
+    apiKey: 2bbff85c4977486096b0e13e56100aed
   }),
 
   FACE_DETECTION: Clarifai.FACE_DETECT_MODEL
